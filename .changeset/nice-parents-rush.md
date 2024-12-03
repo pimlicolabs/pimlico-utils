8a859791cd31c691166ecfda0474f09ba19d501a
Sergey Potekhin <sergey@pimlico.io>
2024-12-03 17:50:50 +0400

    Pim 1306 1 (#3)
    * Update webhook
    
    * Update
    
    * Disable sideEffects
    
    * Update pnpm workspace
    
    * Add changeset
    
    * Remove packageManager
    
    * chore: format

---
+++ .changeset/nice-parents-rush.md
@@ -0,0 +1,6 @@
+---
+"@pimlico/example": patch
+"@pimlico/webhook": patch
+---
+
+Added webhook secret support
--- examples/webhook/api/approve.ts
+++ examples/webhook/api/approve.ts
@@ -1,15 +1,17 @@
 import { pimlicoWebhookVerifier } from "@pimlico/webhook"
 import type { VercelRequest, VercelResponse } from "@vercel/node"
 
-const apiKey = process.env.PIMLICO_API_KEY as string
+const webhookSecret = process.env.PIMLICO_WEBHOOK_SECRET as string
 
-const verifyWebhook = pimlicoWebhookVerifier(apiKey)
+const verifyWebhook = pimlicoWebhookVerifier(webhookSecret)
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-    const body = await verifyWebhook(
+    const webhookEvent = verifyWebhook(
         req.headers as Record<string, string>,
-        Buffer.from(JSON.stringify(req.body))
+        JSON.stringify(req.body)
     )
+
+    // console.log(webhookEvent.data.object.userOperation)
 
     return res.status(200).json({
         sponsor: true
