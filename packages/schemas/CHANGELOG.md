bc929c00b7916873f853c50f4345c8aedbbe6a93
Sergey Potekhin <sergey@pimlico.io>
2025-08-04 12:15:50 +0300

    Pim 1795 (#14)
    * wip
    
    * fix build
    
    * wip
    
    * wip
    
    * Update canary wf
    
    * chore: format
    
    ---------
    
    Co-authored-by: pavlovdog <5871170+pavlovdog@users.noreply.github.com>

---
+++ packages/schemas/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @pimlico/schemas
+
+## 0.0.1
+
+### Patch Changes
+
+- Initial release 
---
+++ packages/schemas/src/rpc.ts
@@ -0,0 +1,34 @@
+import { z } from "zod/v4"
+
+const rpcCallSchema = z
+    .object({
+        jsonrpc: z.literal("2.0"),
+        id: z.number(),
+        method: z.string(),
+        params: z
+            .array(z.unknown())
+            .optional()
+            .transform((val) => val ?? [])
+    })
+    .strict()
+
+const jsonRpcSchema = z.union([rpcCallSchema, z.array(rpcCallSchema)])
+
+type JSONRPCRequest = z.infer<typeof jsonRpcSchema>
+
+const jsonRpcResultSchema = z
+    .object({
+        jsonrpc: z.literal("2.0"),
+        id: z.number(),
+        result: z.unknown()
+    })
+    .strict()
+
+type JSONRPCResponse = z.infer<typeof jsonRpcResultSchema>
+
+export {
+    jsonRpcSchema,
+    jsonRpcResultSchema,
+    type JSONRPCRequest,
+    type JSONRPCResponse
+}
